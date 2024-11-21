70df7cfb33bb24889ce904d75082f210a8e16025
MohammadAlmohsen <muhammadalmohsen8@gmail.com>
2024-11-21 23:23:11 +0200

    Create the first dashboard project using nextjs

---
+++ nextadmin/app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './sidebar.module.css'
+const Sidebar = () => {
+    return (
+        <div className={styles.container}>
+            Sidebar
+        </div>
+    );
+}
+
+export default Sidebar;
