1a530b83ab745c0c04f41dba3a10392e841234a0
MohammadAlmohsen <muhammadalmohsen8@gmail.com>
2024-11-22 17:41:45 +0200

    Start creating the sidebar

--- nextadmin/app/ui/dashboard/sidebar/sidebar.jsx
+++ nextadmin/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import styles from './sidebar.module.css'
+
+const menuItem=[
+    {
+        title:"Pages",
+        list:[
+            {
+                titel:"Dashboard",
+                path:"/dashboard",
+                icon:<MdDashboard/>
+            }
+        ]
+    }
+]
 const Sidebar = () => {
     return (
         <div className={styles.container}>
